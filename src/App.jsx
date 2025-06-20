7c66589d448a67be5edc172c47d9f3fc57a44f88
hds05 <duttsharmahimanshu96@gmail.com>
2025-06-20 20:38:41 +0530

    Refactor App component structure for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ import AddItem from './pages/Additems';
 import ViewItems from './pages/Viewitems';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import './App.css'; 
+import './App.css';
 
 function App() {
   return (
-      <Router className="min-w-screen h-screen flex flex-col items-center justify-center">
-        <nav className='text-gray-500 flex p-5 bg-gray-400 text-white gap-5 sticky top-0' >
-          <Link to="/add" className='bg-black p-3 rounded-xl hover:pointer'>Add Item</Link>
-          <Link to="/view" className='bg-black p-3 rounded-xl hover:pointer'>View Items</Link>
-        </nav>
-        <Routes>
-          <Route path="/add" element={<AddItem />} />
-          <Route path="/view" element={<ViewItems />} />
-        </Routes>
-        <ToastContainer />
-      </Router>
+    <Router className="min-w-screen h-screen flex flex-col items-center justify-center">
+      <nav className='text-gray-500 flex p-5 bg-gray-400 text-white gap-5 sticky top-0' >
+        <Link to="/add" className='bg-black p-3 rounded-xl hover:pointer'>Add Item</Link>
+        <Link to="/view" className='bg-black p-3 rounded-xl hover:pointer'>View Items</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Navigate to="/view" />} />
+        <Route path="/add" element={<AddItem />} />
+        <Route path="/view" element={<ViewItems />} />
+      </Routes>
+      <ToastContainer />
+    </Router>
   );
 }
 
